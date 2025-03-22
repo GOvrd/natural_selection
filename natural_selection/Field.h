b195df8cd3ccd4d981b5fcc1443477637ea3d37a
Dmitry <dimamak.t@gmail.com>
2025-03-23 00:26:03 +0300

    Сделано отображение поля

---
+++ natural_selection/Field.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <iostream>
+#include <ctime>
+
+constexpr char LIVE_CELL = '*';
+
+template<typename CellType>
+class Field {
+private:
+	int _row;
+	int _col;
+	CellType** _field;
+	void init() {
+		srand(time(0));
+		_field = new CellType*[_row];
+		for (int i = 0; i < _row; i++) {
+			_field[i] = new CellType[_col];
+			for (int j = 0; j < _col; j++) {
+				_field[i][j] = rand() % 2;
+			}
+		}
+	}
+	void clear() {
+		for (int i = 0; i < _row; i++) {
+			delete[] _field[i];
+		}
+		delete[] _field;
+	}
+public:
+
+	Field(int, int);
+	~Field();
+	void print();
+};
+template<typename CellType>
+Field<CellType>::Field(int col, int row) {
+	_row = row;
+	_col = col;
+	init();
+}
+template<typename CellType>
+Field<CellType>::~Field() {
+	clear();
+}
+template<typename CellType>
+void Field<CellType>::print() {
+	for (int i = 0; i < _row; i++) {
+		for (int j = 0; j < _col; j++) {
+			if (_field[i][j] == 1) std::cout << LIVE_CELL << " ";
+			else std::cout << "   ";
+		}
+		std::cout << "\n";
+	}
+}
---
+++ natural_selection/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Field.h"
+
+int main() {
+	Field<int> f(10, 10);
+	f.print();
+	return 0;
+}
